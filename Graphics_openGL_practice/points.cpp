1d3b54ebc70bf55061ef5453e083f55dc8732e6f
James Rees <james.rees@outlook.com>
2015-08-15 22:02:08 +0100

    Added points from http://ogldev.atspace.co.uk Note: This does not have shaders so It may or may not work depending on your drivers and how it handles a lack of shaders.

--- Graphics_openGL_practice/points.cpp
+++ Graphics_openGL_practice/points.cpp
@@ -9,18 +9,21 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <SOIL/SOIL.h>
+
+
 using namespace std;
-
-
+using namespace glm;
 
 // Function prototypes
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 void render();
+void initialiseApp();
 // Window dimensions
 const GLuint WIDTH = 800, HEIGHT = 600;
+GLuint VBO;
 
-
-int mains(){
+//GLFWwindow* window;
+int main(){
 	std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
 	// Init GLFW
 	glfwInit();
@@ -50,20 +53,22 @@ int mains(){
 		std::cout << "Failed to initialize GLEW" << std::endl;
 		return -1;
 	}
-
+	// Clear the colorbuffer
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	// Define the viewport dimensions
 	glViewport(0, 0, WIDTH, HEIGHT);
-
+	glPointSize(10);
+	glfwSwapInterval(1);
+	initialiseApp();
 	// Game loop
 	while (!glfwWindowShouldClose(window))
 	{
 		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
 		glfwPollEvents();
-
 		
 		render();
-		// Swap the screen buffers
 		glfwSwapBuffers(window);
+		
 	}
 
 	// Terminate GLFW, clearing any resources allocated by GLFW.
@@ -75,16 +80,37 @@ int mains(){
 void render()
 {
 
-	// Render
-
-	// Clear the colorbuffer
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	//glDrawArrays(GL_POINTS, 0, 1);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDisableVertexAttribArray(0);
+
+	
+
+}
+//initialising app specific openGL draw state
+void initialiseApp()
+{
+	vec3 Vertices[3];
+	Vertices[0] = vec3(-1.0f, -1.0f, 0.0f);
+	Vertices[1] = vec3(1.0f, -1.0f, 0.0f);
+	Vertices[2] = vec3(0.0f, 1.0f, 0.0f);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+	cout << sizeof(Vertices) << endl;
+	cout << Vertices[0].x << endl;
 }
 // Is called whenever a key is pressed/released via GLFW
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
-    std::cout << key << std::endl;
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
+	std::cout << key << std::endl;
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
 }
